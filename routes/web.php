0353ba3bc9ae4882e06d8a60da32c85dee406df1
Benjamin Lowery <loweryb0@shtc2.org.uk>
2017-04-20 19:45:58 +0100

    update route

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,59 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| This file is where outes that are handled, they're are 4 main options a 
+| route can take. These are based on what type of request is sent to the 
+| server. Get is the most common one and used when retrieving data from the
+| server when the route is accessed. post is best used when a form is sent off
+| from the client, this post request is then handled appropriately. Others include,
+| put and delete but these functions can also be handled with a post request so
+| i will not use them for the time being.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Default routes of pages
+Route::get('/', 'HomeController@displayPosts');
+Route::get('/about', 'HomeController@displayAbout');
+Route::get('/post/{slug}', 'postController@display');
+Route::post('/post/{slug}', 'postController@addComment');
+
+// Search routes
+Route::get('/author/{name}', 'postController@author');
+Route::get('/tag/{id}', 'postController@tags');
+Route::get('/search', 'searchController@display');
+Route::post('/search', 'searchController@showResults');
+
+
+// Only allow logged in users on this route
+Route::get('/create', 'createController@editor')->middleware('login:allow');
+Route::post('/create', 'createController@publishPending');
+
+// Admin middleware makes sure we are just dealing with teachers (called admin here)
+Route::group(['middleware' => ['admin']], function () {
+	Route::get('/admin', 'adminController@index');
+	Route::get('/admin/manage', 'adminController@manageUsers');
+	Route::get('/admin/post_review', 'adminController@displayPostReview');
+	Route::get('/admin/acceptPost/{id}', 'adminController@accept');
+	Route::get('/admin/denyPost/{id}', 'adminController@deny');
+	Route::get('/admin/settings', 'adminController@displaySettings');
+	Route::post('/admin/settings', 'adminController@updateSettings');
 });
+
+// Middleware for only students being allowed in this area
+Route::group(['middleware' => ['student']], function () {
+	Route::get('/student', 'studentController@index');
+	Route::get('/student/subscribe/{booleanVal}', 'studentController@handleSub');
+	Route::get('/student/posts', 'studentController@postManagement');
+});
+
+// Routes for login
+// If session is set we redirect the user to another place
+Route::group(['middleware' => 'login:redirect'], function() {
+    Route::get('/login', 'loginController@loginPage');
+	Route::get('/login/verify/{token}', 'loginController@VerifyLogin');
+	Route::post('/login', 'loginController@SendVerify');
+});
+
+
+Route::get('/logout', 'loginController@logout');
