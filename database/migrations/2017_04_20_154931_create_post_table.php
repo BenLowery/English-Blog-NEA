3e838be718f260e6fac4b3e584558719520904c0
Benjamin-Lowery-NEA <loweryb0@shtc2.org.uk>
2017-04-20 19:59:53 +0100

    Dammnit

--- database/migrations/2017_04_20_154931_create_post_table.php
+++ database/migrations/2017_04_20_154931_create_post_table.php
@@ -15,19 +15,19 @@ class CreatePostTable extends Migration
     {
         // Create post table
          Schema::create('post', function (Blueprint $table) {
-          // defines the primary key
-			$table->increments('id');
-			// define as a string (VARCHAR)
-			$table->string('url', 128)->unique();  // Will throw an error in the database if entry isn’t unique
-			$table->string('title', 128)->unique();
-			$table->string('description', 255);
-			// Nullable allows no value to be entered into this field, everything else is not null by default
-			$table->text('tags')->nullable(); 
-			 $table->string('author', 255);
-			// Enum only allows two values which are defined by us into an array, also define a default value
-			 $table->enum('accepted', array('yes', 'no'))->default('no');
-			// Timestamps gives each item an added two fields defining when it was made and last updated
-			 $table->timestamps();
+         // defines the primary key
+	 $table->increments('id');
+	 // define as a string (VARCHAR)
+	$table->string('url', 128)->unique();  // Will throw an error in the database if entry isn’t unique
+	$table->string('title', 128)->unique();
+	$table->string('description', 255);
+	// Nullable allows no value to be entered into this field, everything else is not null by default
+	$table->text('tags')->nullable(); 
+	$table->string('author', 255);
+	// Enum only allows two values which are defined by us into an array, also define a default value
+	$table->enum('accepted', array('yes', 'no'))->default('no');
+	// Timestamps gives each item an added two fields defining when it was made and last updated
+	$table->timestamps();
         });
     }
 
