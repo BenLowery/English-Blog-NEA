87ab65754329610b9e21e6c20e1bf9a83f27b4a3
Benjamin-Lowery-NEA <loweryb0@shtc2.org.uk>
2017-04-23 13:15:40 +0100

    ccc

--- resources/views/student/UserStudent.blade.php
+++ resources/views/student/UserStudent.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 @include('nav')
-	<div class="intro"><h1>Hello, B. Student</h1></div>
+	<div class="intro"><h1>Hello</h1></div>
 	<div class="user__option__container">
 		<div class="flex"><div class="user__option">Manage Email Subscription<br /><br /><a href="#">Subscribe</a>/<a href="#" style="color:#000;">Unsubscribe</a></div></div>
 		<div class="flex"><div class="user__option">Post Management<br /><br /><a href="/student/posts"><i class="fa fa-align-left fa-4x" aria-hidden="true"></i></a></div></div>
